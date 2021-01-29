7eb467c30b699aa77dbda41df9772e1df005e481
Thanh Nguyen <jamesnguyen1st@gmail.com>
2021-01-30 00:07:29 +0700

    flatten returned product response with DTO

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
 using Infrastucture.Data;
 using Core.Entities;
 using Core.Interfaces;
@@ -27,18 +28,37 @@ namespace API.Controllers
 
         [HttpGet]
         // using task to pass the request to a delegate and it does not wait
-        public async Task<ActionResult<List<Product>>>  GetProducts()
+        public async Task<ActionResult<List<ProductToReturnDto>>>  GetProducts()
         {
             var spec = new ProductsWithTypesAndBrandsSpecification();
             var products = await _productsRepository.ListAsync(spec);
-            return Ok(products);
+            return products.Select(product => new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                ProductType = product.ProductType.Name
+            }).ToList();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
-            return await _productsRepository.GetEntityWithSpec(spec);
+            var product = await _productsRepository.GetEntityWithSpec(spec);
+            return new ProductToReturnDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                PictureUrl = product.PictureUrl,
+                Price = product.Price,
+                ProductBrand = product.ProductBrand.Name,
+                ProductType = product.ProductType.Name
+            };
         }
 
         [HttpGet("brands")]
